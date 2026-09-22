1183a2c226f0eeebcfbab1327d29418a70c772e2
Mohammed Hazem <mmosleh993@gmail.com>
2026-09-22 09:10:37 +0300

    env setup

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://blwskzwzfqcflaxpescm.supabase.co";
-const supabaseKey = "sb_publishable_Fn0e51hzGgAAOeP6TSYuoA_838eyAvN";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
